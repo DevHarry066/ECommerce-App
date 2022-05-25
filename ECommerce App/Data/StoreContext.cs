188e97950acdf3556848dcc59b93c9c592be9565
DevHarry066 <sharmahariom066@gmail.com>
2022-05-25 15:53:21 +0530

    Added a context class in data folder

---
+++ ECommerce App/Data/StoreContext.cs
@@ -0,0 +1,20 @@
+﻿using ECommerce_App.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce_App.Data
+{
+    public class StoreContext: DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
