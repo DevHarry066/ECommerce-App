5a8c247998cf0a770aa3336bdc8eb83af13f254e
DevHarry066 <sharmahariom066@gmail.com>
2022-05-25 15:43:59 +0530

    Added product controller

---
+++ ECommerce App/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce_App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "All products list";
+        }
+
+        [HttpGet("{id}")]
+        public string GetProductById(int id)
+        {
+            return "a single product by id";
+        }
+    }
+}
