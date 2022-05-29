8987e5f83145cc4d8fb18003ea92c5bfbef9cd4b
Sharma, Hariom (Contractor) <Hariom_Sharma@comcast.com>
2022-05-29 21:55:14 +0530

    Implemeted Filtering products with Product Brand and Type

--- Core/Specification/ProductWithTypesAndBrandsSpecification.cs
+++ Core/Specification/ProductWithTypesAndBrandsSpecification.cs
@@ -9,7 +9,11 @@ namespace Core.Specification
 {
     public class ProductWithTypesAndBrandsSpecification: BaseSpecification<Product>
     {
-        public ProductWithTypesAndBrandsSpecification(string sort)
+        public ProductWithTypesAndBrandsSpecification(string sort, int? brandId, int? typeId)
+            :base(x=>
+            (!brandId.HasValue || x.ProductBrandId == brandId) &&
+            (!typeId.HasValue || x.ProductTypeId == typeId)
+            )
         {
             AddInclude(p => p.ProductType);
             AddInclude(p => p.ProductBrand);
--- ECommerce App/Controllers/ProductsController.cs
+++ ECommerce App/Controllers/ProductsController.cs
@@ -40,9 +40,9 @@ namespace Core.Controllers
 
         [HttpGet]
         [Route("GetProducts")]
-        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts(string sort)
+        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts(string sort, int? brandId, int? typeId)
         {
-            var spec = new ProductWithTypesAndBrandsSpecification(sort);
+            var spec = new ProductWithTypesAndBrandsSpecification(sort, brandId, typeId);
             var products = await _productRepo.ListAsync(spec);
             if(products != null)
             {
