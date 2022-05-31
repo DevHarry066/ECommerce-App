e3c2762fac4ee3832b5dd166b3d5c97ec558870a
Sharma, Hariom (Contractor) <hariomsharma066@outlook.com>
2022-05-31 18:44:57 +0530

    Display product Brands and Types in Filter part

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ShopService } from './shop.service';
 import { IProduct } from '../shared/models/product';
 import { IPagination } from '../shared/models/pagination';
+import { IBrand } from '../shared/models/brand';
+import { IProductType } from '../shared/models/productTypes';
 
 @Component({
   selector: 'app-shop',
@@ -13,11 +15,37 @@ export class ShopComponent implements OnInit {
   constructor(private shopService: ShopService) { }
 
   products: IProduct[];
+  brands: IBrand[];
+  productTypes: IProductType[];
 
   ngOnInit(): void {
+    this.getProducts();
+    this.getProductBrand();
+    this.getProductTypes();
+  }
+
+  getProducts() {
     this.shopService.getProducts()
       .subscribe((response: IPagination) => {
         this.products = response.data;
+      }, error => {
+        console.log(error);
+      });
+  }
+
+  getProductBrand() {
+    this.shopService.getProductBrand()
+      .subscribe((response: IBrand[]) => {
+        this.brands = response;
+      }, error => {
+        console.log(error);
+      });
+  }
+
+  getProductTypes() {
+    this.shopService.getProductTypes()
+      .subscribe((response: IProductType[]) => {
+        this.productTypes = response;
       }, error => {
         console.log(error);
       });
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { IPagination } from '../shared/models/pagination';
+import { IProductType } from '../shared/models/productTypes';
+import { IBrand } from '../shared/models/brand';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +14,13 @@ export class ShopService {
 
   getProducts() {
     return this.http.get<IPagination>(this.baseUrl + 'products/GetProducts?pageSize=50');
+  }
+
+  getProductBrand() {
+    return this.http.get<IBrand[]>(this.baseUrl + 'Products/GetProductBrands');
+  }
+
+  getProductTypes() {
+    return this.http.get<IProductType[]>(this.baseUrl + 'Products/GetProductTypes');
   }
 }
