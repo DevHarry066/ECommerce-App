afd0181713ec8d82e33145ced617da4b0f06ab61
Sharma, Hariom (Contractor) <hariomsharma066@outlook.com>
2022-05-31 12:52:38 +0530

    Added IProduct and IPagination interface

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,22 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { IProduct } from './models/product';
+import { IPagination } from './models/pagination';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent implements OnInit {
   title = 'ECommerce Application';
-  products: any;
+  products: IProduct[];
 
   constructor(private http: HttpClient) { }
 
-  public ngOnInit(): void {
-    this.http.get("https://localhost:44305/api/products/GetProducts?PageSize=50").subscribe((response: any) => {
-      console.log(response);
-      this.products = response.data;
-    }, error => {
-      console.log(error);
-    });
+  ngOnInit(): void {
+    this.http.get('https://localhost:44305/api/products/GetProducts?pageSize=50')
+      .subscribe((response: IPagination) => {
+        this.products = response.data;
+      }, error => {
+        console.log(error);
+      });
   }
 }
