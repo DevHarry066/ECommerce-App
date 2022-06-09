733b1598a389c3847b2912abc4933a28a6e75b92
DevHarry066 <hariomsharma066@outlook.com>
2022-06-09 16:43:02 +0530

    Displaying Register form on Sign up page

--- client/src/app/account/register/register.component.ts
+++ client/src/app/account/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+
+  constructor(private fb: FormBuilder,
+    private router: Router,
+    private accountService: AccountService) { }
 
   ngOnInit(): void {
+    this.createRegisterForm();
   }
 
+  createRegisterForm() {
+    this.registerForm = this.fb.group({
+      displayName: [null, [Validators.required]],
+      email: [null,
+        [Validators.required, Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')],
+      ],
+      password: [null, [Validators.required]]
+    });
+  }
+
+  onSubmit() {
+    this.accountService.register(this.registerForm.value).subscribe(response => {
+      this.router.navigateByUrl('/account/login');
+    }, error => {
+      console.log(error);
+    });
+  }
 }
